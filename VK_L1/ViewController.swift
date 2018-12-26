613c1daeed222b7cbc0b915e35d41e45b045d4d5
r.chernetsov <r.chernetsov@adonis24.ru>
2018-12-26 19:53:27 +0300

    Initial Commit

---
+++ VK_L1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_L1
+//
+//  Created by Чернецова Юлия on 26/12/2018.
+//  Copyright © 2018 Чернецов Роман. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
